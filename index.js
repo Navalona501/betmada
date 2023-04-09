6edfee590c8a5c0556c155040721113b1e8b3e37
Navalona501 <119346839+Navalona501@users.noreply.github.com>
2023-04-09 20:54:47 +0100

    bet begin

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-console.log(process.env.MONGO_URI)
 var md5 = require('md5');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,12 +10,9 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const PORT = process.env.PORT || 3000;
 
-// const bcrypt = require('bcrypt');
-// const encrypt = require('mongoose-encryption');
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
@@ -42,190 +38,13 @@ const connectDB = async () => {
     console.log(err);
   }
 };
-// mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true }).then(() => {
-//   console.log('MongoDB Connected');
-// }).catch((err) => {
-//   console.log(err);
-// });
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  secret: String
-});
-
-userSchema.plugin(passportLocalMongoose);
-
-const User = new mongoose.model('User', userSchema);
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
   
-  res.render('home');
-});
-
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.get('/secrets', (req, res) => {
-  User.find({ secret: {$ne: null} }).then((users) => {
-    if(users){
-      res.render('secrets', {usersWithSecrets: users});
-    }else{
-      res.render('secrets', {usersWithSecrets: "no secrets yet"});
-    }
-  }).catch((err) => {
-    console.log(err);
+    res.render('home');
   });
-});
-
-app.get('/submit', (req, res) => {
-  if(req.isAuthenticated()){
-    res.render('submit');
-  } else {
-    res.redirect('/login');
-  }
-});
-
-app.post('/submit', (req, res) => {
-  const submittedSecret = req.body.secret;
-  User.findById(req.user.id).then((user) => {
-    if(user){
-      user.secret = submittedSecret;
-      user.save();
-      res.redirect('/secrets');
-    }else{
-      res.redirect('/login');
-    }
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-app.post('/register', (req, res) => {
-  User.register({ username: req.body.username }, req.body.password, (err, user) => {
-    if(err){
-      console.log(err);
-      res.redirect('/register');
-    }else{
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/secrets');
-      });
-    }
-  });
-});
-
-app.post('/login', (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  req.login(user, (err) => {
-    if(err){
-      console.log(err);
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/secrets');
-      });
-    }
-  });
-});
-
-
-
-app.get('/logout', (req, res) => {
-  req.logout(function(err) {
-    if(err) {
-      console.log(err);
-    }
-    res.redirect('/');
-  });
-});
-
-app.route('/api/login').get((req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
   
-  // retourne un access token si succès
-  
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
-})
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000 ');
-// });
-
-// app.post('/register', (req, res) => {
-//   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-//     const newUser = new User({
-//       email: req.body.username,
-//       password: hash
-//     });
-
-//     newUser.save().then(() => {
-//       res.render('secrets');
-//     }).catch((err) => {
-//       console.log(err);
-//     });
-
-//   });
-  
-  
-// });
-
-// app.post('/login', (req, res) => {
-
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   User.findOne({ email: username }).then((user) => {
-//     if (!user) {
-//       res.render('login');
-//     }else{
-//       bcrypt.compare(password, user.password, (err, result) => {
-//         if (result === true) {
-//           res.render('secrets');
-//         } else {
-//           res.render('login');
-//         }
-//       })
-//     }
-//   }).catch((err) => {
-//     console.log(err);
-//   });
-// });
-
-// app.route('/api/login').get((req, res) => {
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   User.findOne({ email: email }).then((user) => {
-//     if(!user){
-//       res.send("User not found");
-//     } else {
-//       bcrypt.compare(password, user.password, (err, result) => {
-//         if (result === true) {
-//           res.send("User connected");
-//         } else {
-//           res.send("Incorrect password");
-//         }
-//       });
-//     }
-    
-//   }).catch((err) => {
-//     console.log(err);
-//   });
-// });
