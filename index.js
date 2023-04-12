f0d53b5f6fed2145011f2f619befa34ca64d87f0
Navalona501 <119346839+Navalona501@users.noreply.github.com>
2023-04-12 21:40:59 +0100

    free tips table done

--- index.js
+++ index.js
@@ -39,9 +39,143 @@ const connectDB = async () => {
   }
 };
 
+let todayTips = [
+  {
+    match: 'Mathare Utd. vs Tusker',
+    tip: '1',
+    result: 'Pending'
+  },
+  {
+    match: 'Admira Prague vs Povltavska FA',
+    tip: '1',
+    result: 'Pending'
+  },
+  {
+    match: 'Bystrice n. Pern. vs Tasovice',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'Chomutov vs Slany',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'AC Milan vs Napoli',
+    tip: 'DCX2',
+    result: 'Pending'
+  },
+  {
+    match: 'Ghazl El Mahallah vs Future FC',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'Cosmos vs Cailungo',
+    tip: '1',
+    result: 'Pending'
+  },
+  {
+    match: 'Tre Fiori vs Juvenes/Dogana',
+    tip: '2',
+    result: 'Pending'
+  }
+];
+
+let yesterdayTips = [
+  {
+    match: 'Millwall U21 vs Swansea U21',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Bristol City U21 vs Ipswich U21',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Young Lions vs DPMM',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Peterborough U21 vs Barnsley U21',
+    tip: '2OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Kinondoni MC vs Geita Gold',
+    tip: 'UN25',
+    result: 'Pending'
+  },
+  {
+    match: 'CSKA 1948 Sofia II vs Svoge',
+    tip: '1',
+    result: 'Pending'
+  },
+  {
+    match: 'BFC Daugavpils vs Valmiera',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'Cotonsport vs Colombe',
+    tip: 'UN25',
+    result: 'Pending'
+  },
+  {
+    match: 'Magra vs MC Alger',
+    tip: 'UN25',
+    result: 'Pending'
+  },
+  {
+    match: 'Kuressaare vs Flora',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'Silkeborg vs Horsens',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'TB Berlin vs Lokomotive Leipzig',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Barracas Central vs Platense',
+    tip: 'UN25',
+    result: 'Pending'
+  },
+  {
+    match: 'L.R. Vicenza vs Juventus U23',
+    tip: 'DNB1',
+    result: 'Pending'
+  },
+  {
+    match: 'Peterhead vs Dunfermline',
+    tip: '2',
+    result: 'Pending'
+  },
+  {
+    match: 'Manchester City vs Bayern Munich',
+    tip: 'OV25',
+    result: 'Pending'
+  },
+  {
+    match: 'Benfica vs Inter',
+    tip: 'DNB1',
+    result: 'Pending'
+  },
+];
+
 app.get('/', (req, res) => {
   
-    res.render('home');
+    res.render('home', {
+      todayTipsp: todayTips,
+      yesterdayTipsp: yesterdayTips
+    });
   });
   
 
